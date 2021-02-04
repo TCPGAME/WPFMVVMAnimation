b93f7e402fda3e7866ee471cb3337f95b91b80f1
Lonner <Lonner@DESKTOP-ADQSFO5>
2021-02-04 14:24:16 +0800

    添加项目文件。

---
+++ WPFMVVMAnimation/UCBAR.xaml.cs
@@ -0,0 +1,74 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows;
+using System.Windows.Controls;
+using System.Windows.Data;
+using System.Windows.Documents;
+using System.Windows.Input;
+using System.Windows.Media;
+using System.Windows.Media.Animation;
+using System.Windows.Media.Imaging;
+using System.Windows.Navigation;
+using System.Windows.Shapes;
+
+namespace WPFMVVMAnimation
+{
+    /// <summary>
+    /// UCBAR.xaml 的交互逻辑
+    /// </summary>
+    public partial class UCBAR : UserControl
+    {
+        //界面的属性名
+        public static readonly DependencyProperty TextProperty = DependencyProperty.Register(
+                "Text", typeof(string), typeof(UCBAR), new PropertyMetadata("属性名"));
+        public double Text
+        {
+            get { return (double)GetValue(TextProperty); }
+            set { SetValue(TextProperty, value); }
+        }
+        //界面矩形的宽
+        public static readonly DependencyProperty ValueProperty = DependencyProperty.Register(
+                "Value", typeof(double), typeof(UCBAR), new PropertyMetadata(30d));
+        public double Value
+        {
+            get { return (double)GetValue(ValueProperty); }
+            set { SetValue(ValueProperty, value); }
+        }
+
+        private double lastValue;
+        DoubleAnimation doubleAnimation;
+        public UCBAR()
+        {
+            InitializeComponent();
+
+            //实例化DoubleAnimation
+            doubleAnimation = new DoubleAnimation()
+            {
+                Duration = new TimeSpan(0, 0, 0, 0, 300),
+            };
+            //动画结束后，保持最后的值
+            doubleAnimation.FillBehavior = FillBehavior.HoldEnd;
+            //动画结束后的事件
+            doubleAnimation.Completed += DoubleAnimation_Completed;
+
+            //重点  创建一个属性监器，并监听ValueProperty改变，如果ValueProperty改变了将触发SelfChanges方法
+            var descriptor = System.ComponentModel.DependencyPropertyDescriptor.FromProperty(UCBAR.ValueProperty, typeof(UCBAR));
+            descriptor.AddValueChanged(this, SelfChanges);
+        }
+
+        private void SelfChanges(object sender, EventArgs e)
+        {
+            doubleAnimation.From = lastValue;
+            doubleAnimation.To = Value;
+            RectView.BeginAnimation(WidthProperty, doubleAnimation);
+        }
+        private void DoubleAnimation_Completed(object sender, EventArgs e)
+        {
+            lastValue = (double)doubleAnimation.To;
+        }
+
+    }
+}
---
+++ WPFMVVMAnimation/VMMainWindow.cs
@@ -0,0 +1,133 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Diagnostics;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Input;
+
+namespace WPFMVVMAnimation
+{
+    public class VMMainWindow : INotifyPropertyChanged
+    {
+        #region 这部分实际开发是放在基类中的，为了方便演示，就放在这列，知道MVVM的不难理解
+        public event PropertyChangedEventHandler PropertyChanged;
+
+        /// <summary>
+        /// 属性发生改变时调用该方法发出通知
+        /// </summary>
+        /// <param name="propertyName">属性名称</param>
+        public void RaisePropertyChanged(string propertyName)
+        {
+            if (PropertyChanged != null)
+            {
+                PropertyChanged(this, new PropertyChangedEventArgs(propertyName));
+            }
+        }
+
+        protected virtual void SetAndNotifyIfChanged<T>(string propertyName, ref T oldValue, T newValue)
+        {
+            if (oldValue == null && newValue == null) return;
+            if (oldValue != null && oldValue.Equals(newValue)) return;
+            if (newValue != null && newValue.Equals(oldValue)) return;
+            oldValue = newValue;
+            RaisePropertyChanged(propertyName);
+        }
+        #endregion
+
+        /*
+         *  以下是两个属性，常规的MVVM ViewModel写法
+         */
+        private string _Text = "分数";
+        private double _Value = 0;
+
+        public string Text 
+        {
+            get 
+            {
+                return _Text;
+            }
+            set 
+            {
+                _Text = value;
+                RaisePropertyChanged("Text");
+            }
+        }
+        public double Value 
+        {
+            get
+            {
+                return _Value;
+            }
+            set
+            {
+                _Value = value;
+                RaisePropertyChanged("Value");
+            }
+        }
+
+
+        //按钮事件
+        public ICommand CmdChangeValue { get { return new RelayCommand<object>(CmdChangeValueAction); } }
+
+        private void CmdChangeValueAction(object obj)
+        {
+            Random random = new Random();
+            Value = (random.Next(0, 2000) / 10.0);//生成0到200带一个小数点的double数
+        }
+    }
+
+    public class RelayCommand<T> : ICommand where T : class
+    {
+        #region 字段
+        readonly Func<T, Boolean> _canExecute;
+        readonly Action<T> _execute;
+        #endregion
+
+        #region 构造函数
+        public RelayCommand(Action<T> execute)
+            : this(execute, null)
+        {
+        }
+
+        public RelayCommand(Action<T> execute, Func<T, Boolean> canExecute)
+        {
+            if (execute == null)
+                throw new ArgumentNullException("execute");
+            _execute = execute;
+            _canExecute = canExecute;
+        }
+        #endregion
+
+        #region ICommand的成员
+        public event EventHandler CanExecuteChanged
+        {
+            add
+            {
+
+                if (_canExecute != null)
+                    CommandManager.RequerySuggested += value;
+            }
+            remove
+            {
+
+                if (_canExecute != null)
+                    CommandManager.RequerySuggested -= value;
+            }
+        }
+
+        [DebuggerStepThrough]
+        public Boolean CanExecute(Object parameter)
+        {
+            return _canExecute == null ? true : _canExecute((T)parameter);
+        }
+
+        public void Execute(Object parameter)
+        {
+            _execute(parameter as T);
+        }
+        #endregion
+    }
+
+}
